fa23b84cb0e387705bbecd1efc08a207182882ae
Ihor Kovalenko <iikovalenko@outlook.com>
2023-01-18 20:22:58 +0100

    dtor structure modification

--- destructor/destructor.cc
+++ destructor/destructor.cc
@@ -1,44 +1,7 @@
-#include <gtest/gtest.h>
+#include "dtor.h"
 #include <memory>
 
-namespace {
-
-class Foo {
-    public:
-    virtual ~Foo(){
-        Foo:destructor_called_ =  true;
-    }
-    static bool destructor_called_;
-};
-
-class Bar : public Foo
-{
-    public:
-    ~Bar(){
-        Bar::destructor_called_ = true;
-    }
-    static bool destructor_called_;
-};
-bool Foo::destructor_called_ = false;
-bool Bar::destructor_called_ = false;
-
-class Base {
-    public:
-    static bool destructor_called_;
-    ~Base(){
-        Base::destructor_called_ = true;
-    }
-};
-
-class Derived : public Base {
-    public:
-    static bool destructor_called_;
-    ~Derived(){
-        Derived::destructor_called_ = true;
-    }
-};
-bool Base::destructor_called_ = false;
-bool Derived::destructor_called_ = false;
+namespace dtor_test {
 
 TEST(Destructor, IsCalledForBothBaseAndDerivedWithVirtual) {
     {
