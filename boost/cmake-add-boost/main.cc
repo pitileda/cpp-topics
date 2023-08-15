4716727887176a060cc0deb625ffda480c137188
Ihor Kovalenko <iikovalenko@outlook.com>
2023-08-15 16:10:08 +0200

    fixup in boost main.cc

--- boost/cmake-add-boost/main.cc
+++ boost/cmake-add-boost/main.cc
@@ -1,18 +1,15 @@
 #include <boost/filesystem.hpp>
 #include <iostream>
-#include <memory>
 
 using namespace boost::filesystem;
 
-int main(int argc, char const *argv[]) {
-  if (argc < 2) {
-    std::cout << "use with an argument\n";
-    return 1;
-  }
+int main(int argc, char const* argv[])
+{
+    if (argc < 2) {
+        std::cout << "use with an argument\n";
+        return 1;
+    }
+    std::cout << file_size(argv[1]) << '\n';
 
-  std::cout << file_size(argv[1]) << '\n';
-  std::cout << "Привет!" << std::endl;
-  std::cout << "F" << std::endl;
-
-  return 0;
+    return 0;
 }
