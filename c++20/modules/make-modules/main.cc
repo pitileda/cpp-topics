8c1e8923f729fe803adb8d7c2722e7f8230aaf95
Igor Kovalenko <ikovale3@ford.com>
2023-09-11 21:34:11 +0200

    c++ modules with Makefile - init

---
+++ c++20/modules/make-modules/main.cc
@@ -0,0 +1,10 @@
+import <iostream>;
+import <vector>;
+import Mathematics;
+
+int main() {
+  std::vector v{1, 3, 7};
+  std::cout << plus(1, 2) << std::endl;
+  std::cout << v[0] << std::endl;
+  return 0;
+}
---
+++ c++20/modules/make-modules/math.cc
@@ -0,0 +1,2 @@
+export module Mathematics;
+export auto plus(auto x, auto y) -> decltype(x + y) { return x + y; }
