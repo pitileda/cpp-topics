e780d2211eeda1e91a227f865eef77fc8c6d7122
Ihor Kovalenko <iikovalenko@outlook.com>
2023-09-12 10:54:55 +0200

    c++20 - concepts - show error message improvements

--- c++20/concepts/SimpleConcept/main.cc
+++ c++20/concepts/SimpleConcept/main.cc
@@ -1,13 +1,12 @@
 #include <concepts>
 #include <iostream>
-#include <ostream>
 #include <type_traits>
 
 template <typename T>
-concept Number = std::is_floating_point_v<T> || std::is_integral_v<T>;
+concept NumberType = std::is_floating_point_v<T> || std::is_integral_v<T>;
 
-template <Number N>
-auto sum(N x, N y) -> decltype(x + y) {
+template <NumberType Number>
+auto sum(Number x, Number y) -> decltype(x + y) {
   return x + y;
 };
 
@@ -15,5 +14,7 @@ int main(int argc, char const *argv[]) {
   auto a = sum(1, 2);
   auto b = sum(1.3, 2.4);
   std::cout << a << std::endl << b << std::endl;
+  // no operand of the disjunction is satisfied
+  // std::cout << sum("hi", "ho") << std::endl;
   return 0;
 }
