57334c193c708b65f30f0242e246e57ed78f1bf9
Igor Kovalenko <iikovalenko@outlook.com>
2024-12-10 10:39:16 +0100

    dbg hackerrank

--- hackerRank/week3/strings_xor.cc
+++ hackerRank/week3/strings_xor.cc
@@ -1,25 +1,21 @@
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
 #include <iostream>
-#include <vector>
+#include <string>
+
 using namespace std;
 
-string strings_xor(string s, string t) {
-  string res = "";
-  for (int i = 0; i < s.size(); i++) {
-    if (s[i] != t[i])
-      res.append("1");
-    else
-      res.append("0");
-  }
-
-  return res;
-}
-
 int main() {
-  string s, t;
-  cin >> s >> t;
-  cout << strings_xor(s, t) << endl;
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  string s1, s2;
+  cin >> s1 >> s2;
+  string res = s1;
+  for (auto i = 0; i < s1.size(); ++i) {
+    if (s1[i] == s2[i]) {
+      res[i] = '0';
+    } else {
+      res[i] = '1';
+    }
+  }
+  cout << res << endl;
+  cout << stoi(string{res[res.size() - 1]});
   return 0;
 }
