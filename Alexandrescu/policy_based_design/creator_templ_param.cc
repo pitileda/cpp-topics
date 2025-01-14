9e53833577105639eb1226f9a5cdde929892a0e2
Igor Kovalenko <iikovalenko@outlook.com>
2025-01-14 11:13:00 +0100

    add protected non-virtual dtor to deny use pointer to base class on derived object

--- Alexandrescu/policy_based_design/creator_templ_param.cc
+++ Alexandrescu/policy_based_design/creator_templ_param.cc
@@ -4,11 +4,24 @@
 #include <type_traits>
 #include <vector>
 
+// One Policy Creator
+// it implements T* create() method
 template <typename T>
 struct NewCreator {
   static T* create() { return new T; }
+
+  // make destructor protected to disable usage like this
+  // typedef WidgetManager<NewCreator> MyWidgetManager;
+  // ...
+  // MyWidgetManager wm;
+  // NewCreator<Widget>* pCreator = &wm; // dubious, but legal
+  // delete pCreator; // compiles fine, but has undefined behavior
+ protected:
+  ~NewCreator() {}
 };
 
+// Another One Policy Creator
+// it implements T* create() method as well
 template <typename T>
 struct MallocCreator {
   static T* create() {
@@ -18,8 +31,14 @@ struct MallocCreator {
     }
     return new (buf) T;
   }
+
+ protected:
+  ~MallocCreator() {}
 };
 
+// And Even more that 2 Policy creators
+// it implements T* create() method
+// but also enrich with 2 more methods
 template <typename T>
 class PrototypeCreator {
  private:
@@ -33,10 +52,6 @@ class PrototypeCreator {
     static_assert(std::is_pointer<decltype(obj)>::value,
                   "Use only prototype version");
   }
-  ~PrototypeCreator() {
-    std::cout << "PC: object " << prototype_ << " to been destroyed\n";
-    delete prototype_;
-  }
 
   T* create() { return prototype_ ? prototype_->clone() : nullptr; }
   T* get() {
@@ -44,6 +59,12 @@ class PrototypeCreator {
     return prototype_;
   }
   void set(T* obj) { prototype_ = obj; }
+
+ protected:
+  ~PrototypeCreator() {
+    std::cout << "PC: object " << prototype_ << " to been destroyed\n";
+    delete prototype_;
+  }
 };
 
 class Widget {
@@ -74,15 +95,17 @@ template <template <typename> class Creator>
 class WidgetManager : public Creator<Widget> {
  private:
   std::vector<Widget*> widgets_;
-  // std::vector<Gadget*> gadgets_;
-  Creator<Widget> widget_creator_;
-  // Creator<Gadget> gadget_creator_;
 
  public:
   WidgetManager() = default;
+  ~WidgetManager() {
+    for (auto widget : widgets_) {
+      delete widget;
+    }
+  }
   bool add() {
     auto size = widgets_.size();
-    widgets_.push_back(widget_creator_.create());
+    widgets_.push_back(this->create());
     std::cout << "address of last widget:" << widgets_.back() << std::endl;
     std::cout << "address of it's vector cell:" << &widgets_.back()
               << std::endl;
@@ -94,6 +117,8 @@ class WidgetManager : public Creator<Widget> {
       std::cout << item << std::endl;
     }
   }
+  using Creator<Widget>::get;
+  using Creator<Widget>::set;
 };
 
 int main(int argc, char const* argv[]) {
@@ -114,6 +139,19 @@ int main(int argc, char const* argv[]) {
   // except common create() for all Creators
   wm.set(new Widget);
   wm.print();
+
+  // The below section is commented after adding
+  // protected dtor in base Policy class
+  // // use WidgetManager on heap
+  // auto pwm = new WidgetManager<PrototypeCreator>();
+  // pwm->add();
+  // pwm->add();
+
+  // // without protected distructor it is possible to convert
+  // // WidgetManager to base class PrototypeCreator<Widget>
+  // PrototypeCreator<Widget>* pCreator = pwm;
+
+  // delete pCreator;  // compiles, but has UB even if it works here
 
   return 0;
 }
