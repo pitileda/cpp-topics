a489ab9d4ddf93cc3e7d436c997eb6cc3f45e709
Ihor Kovalenko <iikovalenko@outlook.com>
2025-06-25 22:51:42 +0200

    Chain of responsibility

---
+++ design_patterns/ChainOfResponsibility/list_pointer_chain.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <list>
+#include <memory>
+#include <ostream>
+#include <utility>
+#include <vector>
+
+template <typename T>
+class Property {
+public:
+  explicit Property(T Value) : value(std::move(Value)) {}
+  explicit operator T() {
+    return value;
+  }
+  Property& operator=(T v) {
+    value = std::move(v);
+    return *this;
+  }
+  T operator*(const T& other) {
+    return value * other;
+  }
+  T operator+(const T& other) {
+    return value + other;
+  }
+  friend std::ostream& operator<<(std::ostream& os, const Property<T>& prop) {
+    return os << prop.value;
+  }
+private:
+  T value;
+};
+
+struct Creature {
+  Creature(const int &Power, const std::string &Name) : power(Power), name(Name) {}
+  Property<int> power;
+  Property<std::string> name;
+friend std::ostream &operator<<(std::ostream &Os, const Creature &Obj) {
+    return Os << "power: " << Obj.power << " name: " << Obj.name;
+  }
+};
+
+class Modificator {
+public:
+  virtual ~Modificator() = default;
+  explicit Modificator(Creature &Next) : creature(Next) {
+    next_modificator = modificators.end();
+  }
+
+  void add(std::unique_ptr<Modificator> Next) {
+    modificators.push_back(std::move(Next));
+  }
+
+  void push_front(std::unique_ptr<Modificator> Next) {
+    modificators.push_front(std::move(Next));
+  }
+
+  virtual bool handle() {
+    for (auto& mod: modificators) {
+      if (!mod->handle()) {
+        return false;
+      }
+    }
+    return true;
+  }
+protected:
+  Creature& creature;
+private:
+  std::list<std::unique_ptr<Modificator>> modificators;
+  std::list<std::unique_ptr<Modificator>>::iterator next_modificator;
+};
+
+class Duplicator : public Modificator {
+public:
+  explicit Duplicator(Creature & Next) : Modificator(Next) {}
+  bool handle() override {
+    creature.power = creature.power * 2;
+    return Modificator::handle();
+  }
+};
+
+class Adder : public Modificator {
+public:
+  explicit Adder(Creature & Next) : Modificator(Next) {}
+  bool handle() override {
+    creature.power = creature.power + 2;
+    // return Modificator::handle();
+    return false;
+  }
+};
+
+int main() {
+  Creature creature(11, "Boo");
+  Modificator root(creature);
+  root.add(std::make_unique<Duplicator>(creature));
+  root.add(std::make_unique<Adder>(creature));
+  root.add(std::make_unique<Duplicator>(creature));
+  root.push_front(std::make_unique<Adder>(creature));
+  root.handle();
+  std::cout << creature.name << creature.power <<std::endl;
+  return 0;
+}
