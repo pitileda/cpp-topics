947f30797c88c50c77e005d0147530a625b5d86e
Ihor Kovalenko <ivkovalenko@gmail.com>
2025-06-26 10:10:11 +0200

    + references polymorphism

--- class/virtual/shape.cc
+++ class/virtual/shape.cc
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+
 class Shape {
  public:
-  virtual int getArea() const = 0;
-  virtual ~Shape(){};
+  virtual double getArea() const = 0;
+  virtual ~Shape() {};
 };
 
 class Rectagle : public Shape {
@@ -16,7 +17,7 @@ class Rectagle : public Shape {
 
  public:
   explicit Rectagle(width w, height h) : w(w), h(h) {}
-  int getArea() const override {
+  double getArea() const override {
     std::cout << "Rec\n";
     return w * h;
   }
@@ -24,22 +25,36 @@ class Rectagle : public Shape {
 
 class Circle : public Shape {
  private:
-  int radius;
+  double radius;
 
  public:
-  explicit Circle(int radius) : radius(radius) {}
-  int getArea() const override {
+  explicit Circle(double radius) : radius(radius) {}
+  double getArea() const override {
     std::cout << "Cir\n";
     return 3.14 * radius * radius;
   }
 };
 
-int main(int argc, char const* argv[]) {
-  std::vector<std::unique_ptr<Shape>> shapes;
+int main() {
+  using ShapePtr = std::unique_ptr<Shape>;
+  std::vector<ShapePtr> shapes;
   shapes.push_back(std::make_unique<Rectagle>(12, 4));
   shapes.push_back(std::make_unique<Circle>(12));
   for (const auto& shape : shapes) {
     std::cout << shape->getArea() << std::endl;
+  }
+
+  std::cout << "Move to references\n";
+
+  // the same with references
+  using ShapeRef = std::reference_wrapper<Shape>;
+  std::vector<ShapeRef> shapes_ref;
+  Rectagle rect(12, 4);
+  Circle circle(12);
+  shapes_ref.push_back(rect);
+  shapes_ref.push_back(circle);
+  for (const auto& shape : shapes_ref) {
+    std::cout << shape.get().getArea() << std::endl;
   }
   return 0;
 }
