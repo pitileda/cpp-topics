9b9079df2203208f643787afcd40026fee278a00
Ihor Kovalenko <ivkovalenko@gmail.com>
2025-06-26 21:32:46 +0200

    add output to show effect in ref structured bindings

--- c++17/structured_bindings_ref.cc
+++ c++17/structured_bindings_ref.cc
@@ -1,11 +1,13 @@
-#include <cstdint>
+#include <iostream>
 
-int main(int argc, char const *argv[])
-{
-	long long theArray[4] = {1ull, 2ull, 3ull, 4ull};
+int main() {
+  long long theArray[4] = {1ull, 2ull, 3ull, 4ull};
 
-	// get the ref-s 
-	auto& [a, b, c, d] = theArray;
-	b = 7ull;
-	return b;
+  // get the ref-s
+  auto& [a, b, c, d] = theArray;
+  b = 7ull;
+  for (const auto& el : theArray) {
+    std::cout << el << std::endl;
+  }
+  return static_cast<int>(theArray[3]);
 }
